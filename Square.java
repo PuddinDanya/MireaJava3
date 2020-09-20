68f553c66f70bdba25466e562ed79e15202ce0db
PuddinDanya <dkansuzyan@mail.ru>
2020-09-20 22:46:01 +0300

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,35 @@
+package Figura;
+import java.math.*;
+public class Square extends Rectangle{
+    private double side;
+    public Square(){
+        this.side = 4;
+        this.color = "green";
+        this.filled = true;
+    }
+    public Square(double side){
+        this.side = side;
+    }
+    public Square(double side, String color, boolean filled){
+        this.side = side;
+        this.color = color;
+        this.filled = filled;
+    }
+    double getSide(){
+        return side;
+    }
+    void setSide(double side){
+        this.side = side;
+    }
+    public void setWidth(double side){
+        this.side = side;
+    }
+    public void setLength(double side){
+        this.side = side;
+    }
+    @Override
+    public String toString(){
+        return "Shape: square, side: "+this.side+", color: "+this.color+", filled: "+this.filled;
+    }
+
+}
---
+++ TestMovable.java
@@ -0,0 +1,10 @@
+package Figura;
+
+public class TestMovable {
+    public static void main(String[] args) {
+    MovablePoint m1 = new MovablePoint(1, 2 ,2, 2);
+    MovableCircle m2 = new MovableCircle(1, 2 ,2, 2);
+    System.out.println(m1);
+    System.out.println(m2);
+    }
+}
